4b4f73123ea97f6a5cf63fbb5f7ca36d1ed8dfcd
r476 <39871428+r476@users.noreply.github.com>
2020-06-15 12:40:12 +0700

    Добавил логирование сообщений в modbus_doc.py

--- modbus_doc.py
+++ modbus_doc.py
@@ -1,10 +1,10 @@
-﻿from modbus.client import *
+from modbus.client import *
 import csv, time, datetime, telebot
 from telebot import apihelper
 from time import sleep
 
 id_list = [723253749, 
-           723253748]
+           1036253569]
 
 titles = ['Дата Время', 
           'ГПГУ 1 ', 
@@ -28,11 +28,13 @@ data_file = "data.csv"
 #    writer = csv.writer(f)
 #    writer.writerow(titles)
 
+# определяю знак числа
 def number_sing(n):
     return (n-65535) if n & 0b1000000000000000 else n
 
 # получаю данные
 def get_data():
+    data_dict = {}
     try:
         c = client(host="192.168.127.254", unit=7)
         
@@ -63,34 +65,40 @@ def to_csv(data_file, order, data):
         writer = csv.DictWriter(f, order)
         writer.writerow(data)
 
+def log_to_csv(text):
+   with open('msglog.log', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(text)
+
+def send_messages(id_list, text):
+    try:
+        for i in id_list:
+            bot.send_message(i, text)
+            log_to_csv(text)
+            print(text)
+    except:
+        print('Неудачная отправка сообщения')
+        sleep(30)
+  
 data_dict_old = get_data()
 
 # Основной цикл
 while True:
-    data_dict_new = get_data()
+    data_dict_new = get_data() if get_data() else data_dict_old # если данные не получены, то оставляю старые значения 
     
     for k in ['ГПГУ 1 ', 'ГПГУ 2 ', 'ГПГУ 3 ', 'ГПГУ 4 ', 'ГПГУ 5 ']:
         if data_dict_old[k] > 0 and data_dict_new[k] <= 0:
-            print(f'{k} остановлена {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}')
-            try:
-                bot.send_message(723253749, f'{k} остановлена {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}')
-            except:
-                print('Неудачная отправка сообщения')
-            sleep(30)
+            text = f'{k} остановлена {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}'
+            send_messages(id_list, text)
+         
         if data_dict_old[k] <= 0 and data_dict_new[k] > 0:
-            print(f'{k} включена в работу {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}')            
-            try:
-                bot.send_message(723253749, f'{k} включена в работу {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}')
-            except:
-                print('Неудачная отправка сообщения')                
-            sleep(30)
+            text = f'{k} включена в работу {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}'
+            send_messages(id_list, text)
             
     if data_dict_old['BIN'] != data_dict_new['BIN']:
-        print(f'Похоже, что отработал МСВ. {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}')            
-        try:
-            bot.send_message(723253749, f'Похоже, что отработал МСВ. {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}')
-        except:
-            print('Неудачная отправка сообщения')
+        text = f'Похоже, что отработал МСВ. {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}'
+        send_messages(id_list, text)
+            
     to_csv(data_file, titles, data_dict_new)
     data_dict_old = data_dict_new
     time.sleep(5)
