30e1b55499276440e268da5ce6f0a9a75c1454af
r476 <39871428+r476@users.noreply.github.com>
2020-06-20 15:30:27 +0700

    Добавил исключения и вообще причесал немного код

--- doc_bot.py
+++ doc_bot.py
@@ -1,5 +1,5 @@
 from modbus.client import *
-import csv, time, datetime, telebot, json, requests
+import csv, time, datetime, telebot, json, requests, shutil
 import pandas as pd
 from pandas.tseries.offsets import Hour, Minute, Day
 import matplotlib.pyplot as plt 
@@ -12,16 +12,18 @@ def get_updates():
         updates = json.load(f)
         for u in updates['result']:
             updates_list.append(u['update_id'])
-            
-    method = 'getUpdates'
-    r = requests.get(f'https://api.telegram.org/bot{token}/{method}')
-            
-    for u in r.json()['result']:
-        if u['update_id'] not in updates_list:
-            return_json.append(u)
-        
-    with open('req.json', 'w') as f:
-        json.dump(r.json(), f, indent=4)
+    try:
+        method = 'getUpdates'
+        r = requests.get(f'https://api.telegram.org/bot{token}/{method}')
+
+        for u in r.json()['result']:
+            if u['update_id'] not in updates_list:
+                return_json.append(u)
+
+        with open('req.json', 'w') as f:
+            json.dump(r.json(), f, indent=4)
+    except Exception as e:
+        print(e)
         
     return return_json
 
@@ -58,7 +60,7 @@ def get_data():
             'BIN': b_in}
     except:
         print('Неудачная попытка опроса IM.')
-        sleep(10)
+    
     return data_dict
 
 # запись данных в CSV
@@ -81,37 +83,40 @@ def send_messages(id_list, text):
             log_to_csv(text)
     except:
         print('Неудачная отправка сообщения')
-        sleep(30)
 
 def make_graph(mean_int, interval):
-    df = pd.read_csv('data.csv', parse_dates=['Дата Время'], index_col=['Дата Время'])
+    try:
+        df = pd.read_csv(data_file, parse_dates=['Дата Время'], index_col=['Дата Время'])
 
-    data_mean = df.resample(mean_int).mean()
-    data_sample = data_mean[data_mean.index[-1]-interval:]
+        data_mean = df.resample(mean_int).mean()
+        data_sample = data_mean[data_mean.index[-1]-interval:]
 
-    plt.figure(figsize=(12,6))
-    plt.ylim([-1000, 7000])
-    plt.ylabel('кВт')
-#    plt.xticks(rotation=45)
-    plt.grid(True)
-    plt.plot(data_sample.index, data_sample['Мощность завода'], 'r-')
-    plt.plot(data_sample.index, data_sample['Сумм мощность ГПГУ'], 'g-')
-    plt.plot(data_sample.index, data_sample['MainsImport'], 'b-')
-    plt.axhline(y=data_sample['Мощность завода'].mean(), alpha=0.5, color='r')
-    plt.axhline(y=data_sample['MainsImport'].mean(), alpha=0.5, color='b')
-    plt.axhline(y=data_sample['Сумм мощность ГПГУ'].mean(), alpha=0.5, color='g')
-    plt.legend(['Завод', 'ГПГУ', 'Импорт'])
-    plt.figtext(.13, .96, f'Средняя мощность завода на выбранном интервале: {round(data_sample["Мощность завода"].mean())} кВт')
-    plt.figtext(.13, .93, f'Средняя мощность ГПГУ на выбранном интервале: {round(data_sample["Сумм мощность ГПГУ"].mean())} кВт.   Выработано {int(data_sample["MWh"][-1]-data_sample["MWh"][0])} кВт ч')
-    plt.figtext(.13, .9, f'Средний импорт на выбранном интервале: {round(data_sample["MainsImport"].mean())} кВт')
-    plt.savefig('1.png')
-
+        plt.figure(figsize=(12,6))
+        plt.ylim([-1000, 7000])
+        plt.ylabel('кВт')
+    #    plt.xticks(rotation=45)
+        plt.grid(True)
+        plt.plot(data_sample.index, data_sample['Мощность завода'], 'r-')
+        plt.plot(data_sample.index, data_sample['Сумм мощность ГПГУ'], 'g-')
+        plt.plot(data_sample.index, data_sample['MainsImport'], 'b-')
+        plt.axhline(y=data_sample['Мощность завода'].mean(), alpha=0.5, color='r')
+        plt.axhline(y=data_sample['MainsImport'].mean(), alpha=0.5, color='b')
+        plt.axhline(y=data_sample['Сумм мощность ГПГУ'].mean(), alpha=0.5, color='g')
+        plt.legend(['Завод', 'ГПГУ', 'Импорт'])
+        plt.figtext(.13, .96, f'Средняя мощность завода на выбранном интервале: {round(data_sample["Мощность завода"].mean())} кВт')
+        plt.figtext(.13, .93, f'Средняя мощность ГПГУ на выбранном интервале: {round(data_sample["Сумм мощность ГПГУ"].mean())} кВт.   Выработано {int(data_sample["MWh"][-1]-data_sample["MWh"][0])} кВт ч')
+        plt.figtext(.13, .9, f'Средний импорт на выбранном интервале: {round(data_sample["MainsImport"].mean())} кВт')
+        plt.savefig('1.png')
+    except Exception as e:
+        shutil.copyfile('fail.png', '1.png')
+        print(e)
+    
 # отправка отчета в виде графика с подписями
 def send_report(id_list):
     try:
         make_graph('2T', Hour(24))
         for i in id_list:
-            url = "https://api.telegram.org/bot827576612:AAEX0IHqMW5x-oWrh8T1ZXhE-9_K8pXMTJ0/sendPhoto"
+            url = f"https://api.telegram.org/bot{token}/sendPhoto"
             files = {'photo': open('1.png', 'rb')}
             data = {'chat_id' : i}
             requests.post(url, files=files, data=data)
@@ -126,19 +131,7 @@ def send_report(id_list):
     except Exception as e:
         print('\n\nНеудачная отправка send_report\n\n')
         print(e)
-        sleep(10)
-        
-# отправка изображений
-def send_photo(id_list, text):
-    try:
-        for i in id_list:
-            url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={i}&text={text}'
-            r= requests.post(url)
-            log_to_csv(text)
-    except:
-        print('Неудачная отправка сообщения')
-        sleep(30)
-        
+               
 def reset_report_marker():
     with open('config.json', 'r') as f:
         jdata = json.load(f)
@@ -152,19 +145,60 @@ def is_report_marker_on():
         jdata = json.load(f)
         return False if not jdata['report_today'] else True
     
+def send_graph(message_id):
+    url = f"https://api.telegram.org/bot{token}/sendPhoto"
+    files = {'photo': open('1.png', 'rb')}
+    data = {'chat_id' : message_id}
+    try:
+        requests.post(url, files=files, data=data)
+    except Exception as e:
+        print(e)
+    
 def handler_updates(message):
     message_id = message['message']['from']['id']
     text = message['message']['text']
     print(message_id)
     print(text)
+    
     if text == '/wtf':
         log_to_csv(str(message))
         df = pd.read_csv(data_file, parse_dates=['Дата Время'], index_col=['Дата Время'])[-5:]
         text = f"*{datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')}*\n\n*ГПГУ 1: *{int(df['ГПГУ 1 '].mean())} кВт\n*ГПГУ 2: *{int(df['ГПГУ 2 '].mean())} кВт\n*ГПГУ 3: *{int(df['ГПГУ 3 '].mean())} кВт\n*ГПГУ 4: *{int(df['ГПГУ 4 '].mean())} кВт\n*ГПГУ 5: *{int(df['ГПГУ 5 '].mean())} кВт\n\n*Мощность завода: *{int(df['Мощность завода'].mean())} кВт\n*Сумм мощность ГПГУ: *{int(df['Сумм мощность ГПГУ'].mean())} кВт\n*Импорт: *{int(df['MainsImport'].mean())} кВт\n\n*MWh: *{df['MWh'][-1]}"
         print(text)
         send_messages([message_id], text)
+        
+    if text == '/get_data_3hour':
+        make_graph('T', Hour(3))
+        send_graph(message_id)
+        
+    if text == '/get_data_12hour':
+        make_graph('2T', Hour(12))
+        send_graph(message_id)
 
-id_list = [723253749] #get_id_list()
+    if text == '/get_data_24hour':
+        make_graph('2T', Hour(24))
+        send_graph(message_id)
+
+    if text == '/get_data_3days':
+        make_graph('5T', Day(3))
+        send_graph(message_id)
+
+    if text == '/get_data_7days':
+        make_graph('10T', Day(7))
+        send_graph(message_id)
+
+    if text == '/get_data_14days':
+        make_graph('10T', Day(14))
+        send_graph(message_id)
+
+    if text == '/get_data_30days':
+        make_graph('10T', Day(30))
+        send_graph(message_id)
+       
+    log_to_csv(str(message))
+#         bot.send_message(723253749, f'Сообщение от {message['message']['from']['first_name']}\n{message['message']['from']['id']}\n{message['text']}')
+
+id_list = get_id_list()
 
 titles = ['Дата Время', 
           'ГПГУ 1 ', 
@@ -178,8 +212,8 @@ titles = ['Дата Время',
           'Сумм мощность ГПГУ', 
           'BIN']
 
-#token = '1298999210:AAHQXHgqW0y0A9kjCPB3XSeBZKDNrgmK9fY'
-token = '827576612:AAEX0IHqMW5x-oWrh8T1ZXhE-9_K8pXMTJ0'
+token = '1298999210:AAHQXHgqW0y0A9kjCPB3XSeBZKDNrgmK9fY'
+#token = '827576612:AAEX0IHqMW5x-oWrh8T1ZXhE-9_K8pXMTJ0'
 data_file = "data.csv"
 
 # блок кода для инициации файла csv
